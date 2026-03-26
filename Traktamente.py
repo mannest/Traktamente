6128a56224879faade1bd3624aec1bfef28cf157
Manne <manne.stenstrom@gmail.com>
2026-03-26 19:04:43 +0100

    Add files via upload

---
+++ Traktamente.py
@@ -0,0 +1,37 @@
+"""En traktamentes räknare åt rikstaxens fältpersonal"""
+import datetime as dt
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET","POST"])
+def index():
+    
+    #Konstanter
+    error_time = None
+    error_datum = None
+    differens = None
+
+    if request.method == "POST":
+        startdate = request.form["startdate"]
+        starttime = request.form["starttime"]
+        enddate = request.form["enddate"]
+        endtime = request.form["endtime"]
+
+        if not startdate or not enddate:
+            return "alla datum måste fyllas i"       
+        elif not starttime or not endtime:
+            return "Start eller slut tid ej ifyllt"
+        else:
+            start = dt.date.fromisoformat(startdate)
+            end = dt.date.fromisoformat(enddate)
+
+            differens = (end - start).days +1
+            #print(differens)
+            return str(differens)
+
+    return render_template(
+            'index.html', )
+    
+if __name__ == "__main__":
+    app.run(debug=True)
